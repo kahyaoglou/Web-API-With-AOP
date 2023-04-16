238780c8003a3bb22cdd2745bfbadce9e9ef68eb
Furkan.Kahyaoglu <Furkan.Kahyaoglu@FURKANKHP>
2023-04-16 18:29:06 +0300

    Users class ve UsersController class eklendi.

---
+++ Web-API-With-AOP/Controllers/UsersController.cs
@@ -0,0 +1,36 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+
+namespace Web_API_With_AOP.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+        public UsersController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Users>>> GetAllUsers()
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            var users = await connection.QueryAsync<Users>("SELECT * FROM tbl_clubUsers");
+            return Ok(users);
+
+        }
+
+        [HttpGet("{Id}")]
+        public async Task<ActionResult<List<Users>>> GetUser(int Id)
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            var user = await connection.QueryFirstAsync<Users>("SELECT * FROM tbl_clubUsers WHERE userID = @Id", new { Id = Id });
+            return Ok(user);
+
+        }
+    }
+}
---
+++ Web-API-With-AOP/Users.cs
@@ -0,0 +1,12 @@
+﻿namespace Web_API_With_AOP
+{
+    public class Users
+    {
+        public int Id { get; set; }
+        public int Department { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Position { get; set; }
+        public string City { get; set; }
+    }
+}
